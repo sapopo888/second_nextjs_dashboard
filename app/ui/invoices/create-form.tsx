b16af0c3d603bf2547ad608fa2a584fbd8441aa1
sapopo888 <sapopo0813@yahoo.co.jp>
2025-11-19 11:06:58 +0900

    customer選択欄にエラーメッセージ表示

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -15,6 +15,8 @@ import { useActionState } from 'react';
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = { message: null, errors: {} };
   const [state, formAction] = useActionState(createInvoice, initialState);
+  console.log('state', state);
+
 
   return (
     <form action={formAction}>
@@ -30,6 +32,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby='customer-error'
             >
               <option value="" disabled>
                 Select a customer
@@ -41,6 +44,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+          </div>
+          <div id='customer-error' aria-live='polite' aria-atomic='true'>
+            {state.errors?.customerId &&
+            state.errors.customerId.map((error: string) => (
+              <p className='mt-2 text-sm text-red-500' key={error}>
+                {error}
+              </p>
+            ))}
           </div>
         </div>
 
